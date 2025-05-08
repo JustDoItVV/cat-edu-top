f4d70dd35688e4f452fb979f34ffdeb35e551124
JustDoItVV <vadim.v.antipov@gmail.com>
2025-05-08 11:48:09 +0300

    fix: animations

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,32 +1,25 @@
 import cn from 'classnames';
-import {
-    AnimationControls, motion, TargetAndTransition, VariantLabels, Variants
-} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { DetailedHTMLProps, ForwardedRef, forwardRef, HTMLAttributes } from 'react';
 
 import styles from './Card.module.css';
 
 interface CardProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   cardColor?: 'white' | 'blue';
-  animate?: boolean | AnimationControls | TargetAndTransition | VariantLabels | undefined;
-  variants?: Variants | undefined;
-  initial?: boolean | TargetAndTransition | VariantLabels | undefined;
 }
 
-export const Card = forwardRef(
+export const Card = motion.create(forwardRef(
   function Card({ cardColor = 'white', className, children, ...props }: CardProps, ref: ForwardedRef<HTMLDivElement>): React.JSX.Element {
     return (
-      <motion.div>
-        <div
-          ref={ref}
-          className={cn(styles.card, className, {
-            [styles.blue]: cardColor === 'blue',
-          })}
-          {...props}
-        >
-          {children}
-        </div>
-      </motion.div>
+      <div
+        ref={ref}
+        className={cn(styles.card, className, {
+          [styles.blue]: cardColor === 'blue',
+        })}
+        {...props}
+      >
+        {children}
+      </div>
     );
   }
-);
+));
--- src/page-components/ProductComponent/ProductComponent.tsx
+++ src/page-components/ProductComponent/ProductComponent.tsx
@@ -13,7 +13,7 @@ interface ProductComponentProps {
   firstCategory?: PageCategory;
 }
 
-export const ProductComponent = forwardRef(
+export const ProductComponent = motion.create(forwardRef(
   function ProductComponent({ page, products, firstCategory }: ProductComponentProps, ref: ForwardedRef<HTMLDivElement>) {
     const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(sortReducer, { products, sort: SortOptions.Rating });
 
@@ -26,33 +26,31 @@ export const ProductComponent = forwardRef(
     };
 
     return (
-      <motion.div>
-        <div ref={ref} className={styles.wrapper}>
-          <div className={styles.title}>
-            <HTag tag='h1'>{page.title}</HTag>
-            {products && <Tag color='grey' size='m' aria-label={products.length + ' элементов'}>{products.length}</Tag>}
-            <Sort sort={sort} setSort={setSort} />
-          </div>
-          <div>
-            {sortedProducts && sortedProducts.map((product) => (
-              <Product key={product._id} product={product} /> ))}
-          </div>
-          <div className={styles.hhTitle}>
-            <HTag tag='h2'>Вакансии - {page.category}</HTag>
-            <Tag color='red' size='m'>hh.ru</Tag>
-          </div>
-          {firstCategory === PageCategory.Courses && page.hh && <HhData {...page.hh} />}
-          {page.advantages && page.advantages.length > 0 && (
-            <>
-              <HTag tag='h2'>Преимущества</HTag>
-              <Advantages advantages={page.advantages} />
-            </>
-          )}
-          {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
-          <HTag tag='h2'>Получаемые навыки</HTag>
-          {page.tags.map((tag) => <Tag key={tag} color='primary'>{tag}</Tag>)}
+      <div ref={ref} className={styles.wrapper}>
+        <div className={styles.title}>
+          <HTag tag='h1'>{page.title}</HTag>
+          {products && <Tag color='grey' size='m' aria-label={products.length + ' элементов'}>{products.length}</Tag>}
+          <Sort sort={sort} setSort={setSort} />
         </div>
-      </motion.div>
+        <div>
+          {sortedProducts && sortedProducts.map((product) => (
+            <Product key={product._id} product={product} /> ))}
+        </div>
+        <div className={styles.hhTitle}>
+          <HTag tag='h2'>Вакансии - {page.category}</HTag>
+          <Tag color='red' size='m'>hh.ru</Tag>
+        </div>
+        {firstCategory === PageCategory.Courses && page.hh && <HhData {...page.hh} />}
+        {page.advantages && page.advantages.length > 0 && (
+          <>
+            <HTag tag='h2'>Преимущества</HTag>
+            <Advantages advantages={page.advantages} />
+          </>
+        )}
+        {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText }} />}
+        <HTag tag='h2'>Получаемые навыки</HTag>
+        {page.tags.map((tag) => <Tag key={tag} color='primary'>{tag}</Tag>)}
+      </div>
     );
   }
-);
+));
